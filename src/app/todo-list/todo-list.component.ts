8bf9f539441f148a6b2a83c9d1fd5bc9f597bb0a
Nimisha Wilson <nimishawilson123@gmail.com>
2020-07-24 00:22:37 +0530

    feat: add functionality of marking a note as done

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,13 +3,58 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  isNoteChecked: boolean;
+  toDoListData: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.toDoListData = [
+      {
+        id: 1,
+        title: 'maths',
+        description:
+          'sfsff sfjfsfs fjks afmssmfkmsdkfkf smfkmsdkfkf jfnjsdnf sdjfnsdf sjfndfns',
+        complete: false,
+      },
+      {
+        id: 2,
+        title: 'sjdnf',
+        description:
+          'loriujm ipsum jnjso sf t iis fs fs dfjdssmfkmsdkfkf  jfnsdf sjfndfns dfsndfsdfn',
+        complete: false,
+      },
+      {
+        id: 3,
+        title: 'dfjsdfj',
+        description:
+          'sfsff sfjfsfs fjks afmssmfkmsdkfkf smfkmsdkfkf jfnjsdnf sdjfnsdf sjfndfns',
+        complete: true,
+      },
+      {
+        id: 4,
+        title: 'social',
+        description:
+          'sfsff sfjfsfs fjks afmssmfkmsdkfkf smfkmsdkfkf jfnjsdnf sdjfnsdf sjfndfns',
+        complete: false,
+      },
+    ];
   }
 
+  markAsDone(event, id) {
+    this.isNoteChecked = event.checked;
+    this.updateCheckedStatus(id, this.isNoteChecked);
+  }
+
+  updateCheckedStatus(id, checkedStatus: boolean) {
+    for (let i in this.toDoListData) {
+      if (this.toDoListData[i].id == id) {
+        this.toDoListData[i].complete = checkedStatus;
+        break;
+      }
+    }
+  }
 }
